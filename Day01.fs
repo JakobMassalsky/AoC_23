fe20a4da1bf04128eab8825d18b8ab5571c2f3df
Jakob Massalsky <jakob.massalsky@gmail.com>
2023-12-01 11:00:21 +0100

    day01

--- Day01.fs
+++ Day01.fs
@@ -1,16 +1,34 @@
 module Day01
 
 open Utils
+open System
+
+let filterOutDigits1 (s:string) : string =
+    let s' = String.filter Char.IsDigit s
+    string s'.[0] + string s'.[s'.Length-1]
 
 // Function for the first part of the day's problem
-let solvePart1 (input) : string =
-    // Your solution for part 1 goes here
-    "Not implemented"
+let solvePart1 (input: string array) : string =
+    input |> Array.map filterOutDigits1
+        |> Array.map Int32.Parse
+        |> Array.sum
+        |> string
+
+
+open System.Text.RegularExpressions
+
+let filterOutDigits2 (s:string) : string =
+    let s' = s.Replace("one", "o1e").Replace("two", "t2o").Replace("three", "t3e").Replace("four", "f4r").Replace("five", "f5e").Replace("six", "s6x").Replace("seven", "s7n").Replace("eight", "e8t").Replace("nine", "n9e")
+
+    let s'' = String.filter Char.IsDigit s'
+    string s''.[0] + string s''.[s''.Length-1]
 
 // Function for the second part of the day's problem
 let solvePart2 (input) : string =
-    // Your solution for part 2 goes here
-    "Not implemented"
+    input |> Array.map filterOutDigits2
+        |> Array.map Int32.Parse
+        |> Array.sum
+        |> string
 
 // Main function to run your solutions
 let solve =
